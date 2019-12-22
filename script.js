b6d69108150ff6eaef395ffb69f315dd066286fc
realVato <44781061+realVato@users.noreply.github.com>
2019-12-22 19:09:01 +0200

    Slider script

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 // Side Navbar
 const sideNav = document.querySelector('.sidenav');
 M.Sidenav.init(sideNav, {});
+
+// Slider
+const slider = document.querySelector('.slider');
+M.Slider.init(slider, {
+    indicators: false,
+    height: 700,
+    duration: 500,
+    interval: 4000
+});
