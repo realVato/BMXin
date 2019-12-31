0fe6e773f50c4b3b0760fa07d964059bc22f5dcd
realVato <44781061+realVato@users.noreply.github.com>
2019-12-31 11:53:51 +0200

    Slider removed

--- script.js
+++ script.js
@@ -2,15 +2,6 @@
 const sideNav = document.querySelector('.sidenav');
 M.Sidenav.init(sideNav, {});
 
-// Slider
-const slider = document.querySelector('.slider');
-M.Slider.init(slider, {
-    indicators: false,
-    height: 700,
-    duration: 500,
-    interval: 4000
-});
-
 // AutoComplete
 const autoComplete = document.querySelector('.autocomplete');
 M.Autocomplete.init(autoComplete, {
